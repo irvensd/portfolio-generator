644923d76549c88835994ef3688eb5d332e7f42f
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-01-16 20:16:53 -0500

    Generated HTML page

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length); 
-// console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js')
 
+const profileDataArgs = process.argv.slice(2);
 
+const [userName, githubName] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i <profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    } 
+fs.writeFile("index.html", generatePage(userName, githubName), err => {
+  if (err) throw new Error (err);
 
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
 
-    profileDataArr.forEach(profileItem =>  console.log(profileItem));
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // console.log(profileDataArgs);
 
-}
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i <profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
 
-printProfileData(profileDataArgs)
+//     profileDataArr.forEach(profileItem =>  console.log(profileItem));
+
+// }
+
+// printProfileData(profileDataArgs)
