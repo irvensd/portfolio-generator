b8a50c1f4b230fb7a41158ed199c4742e146330d
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-01-16 22:09:48 -0500

    Created Gitignore file

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+ .prompt([
+   {
+     type: 'input',
+     name: 'name',
+     message: 'What is your name?'
+   }
+ ])
+ .then(answers => console.log(answers)); 
 
-const [userName, githubName] = profileDataArgs;
 
-fs.writeFile("index.html", generatePage(userName, githubName), err => {
-  if (err) throw new Error (err);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js')
+
+// const pageHTML = generatePage(userName, githubName);
+
+// fs.writeFile("index.html", pageHTML, err => {
+//   if (err) throw new Error (err);
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+
 
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // // console.log(profileDataArgs);
